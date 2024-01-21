3a3bf36e651ed4f6039f99e47583daf0a73ede73
Thomas Gustafson <gus.duckfan@gmail.com>
2024-01-20 20:18:37 -0800

    Implement function to get user input and play a round

--- source.js
+++ source.js
@@ -14,3 +14,43 @@ function getComputerChoice(){
             break;
     }
 }
+
+function getUserChoice(){
+    let choice = prompt("Please enter Rock, Paper or Scissors")
+
+    if(choice.toLowerCase(choice)!='rock' && choice.toLowerCase(choice)!='paper' && choice.toLowerCase(choice)!='scissors'){
+        console.log("Invalid selection")
+        return getUserChoice()
+    }
+    return choice
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "You win! Rock beats Scissors"
+    }
+    else if(playerSelection == "rock" && computerSelection == "paper"){
+        return "You lose! Paper beats Rock"
+    }
+    else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return "You lose! Rock beats Scissors"
+    }
+    else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return "You win! Scissors beats Paper"
+    }
+    else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "You Win! Paper beats Rock"
+    }
+    else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "You lose! Scissors beats Paper"
+    }
+    else{
+        console.log("It's a tie")
+        return playRound(getComputerChoice(),getUserChoice())
+    }
+
+}
+
+console.log(playRound(getComputerChoice(),getUserChoice()))
